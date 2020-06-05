b0423a41fa1897a797546075558bc37932244518
deeksha-punachithaya <44801977+deeksha-punachithaya@users.noreply.github.com>
2020-06-05 20:49:56 +0530

    Update KNN_Assignment.py

--- Assignment_KMeansKNN/KNN_Assignment.py
+++ Assignment_KMeansKNN/KNN_Assignment.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[73]:
-
-
 import random
 import numpy as np
 import pandas as pd 
@@ -12,10 +6,6 @@ import math
 import operator
 from sklearn.preprocessing import StandardScaler
 from sklearn import preprocessing
-
-
-# In[74]:
-
 
 class KNN:
     def __init__(self, metric='euclidean', k=6, p=3):
@@ -110,14 +100,8 @@ class KNN:
             arr.append(accuracy)
         print("Best k is :",arr.index(max(arr)) + 1)
         return arr
-    
-    
 
-
-# # Shuffling and splitting the dataset into test and train data 
-
-# In[75]:
-
+# Shuffling and splitting the dataset into test and train data 
 
 df = pd.read_csv('diabetes.csv')
 print("the diabetes dataset used consists of ",df.shape," number of rows and columns")
@@ -134,38 +118,17 @@ print("testing set :",len(test_set)," rows")
 trainset_List=train_set.values.tolist()
 testset_list=test_set.values.tolist()
 
-
-# In[76]:
-
-
 # Predicting without Pre-Processing the dataset, using euclidean distance measure and k=6
-
-
-# In[77]:
-
 
 model = KNN()
 predictions = model.knn(trainset_List, testset_list)
-
-
-# In[78]:
-
-
 arr = model.check_best_k()
-
-
-# In[79]:
-
-
 k_arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 plt.figure()
 plt.plot(k_arr, arr)
+plt.show()
 
-
-# # Pre-Processing the dataset using standard scalar
-
-# In[80]:
-
+# Pre-Processing the dataset using standard scalar
 
 df = pd.read_csv('diabetes.csv')
 column_names = list(df.columns)
@@ -173,7 +136,6 @@ del column_names[-1]
 print("Data without Pre-Processing\n")
 print(df)
 print("\n")
-
 
 std = StandardScaler()
 standard = std.fit_transform(df[['Pregnancies','Glucose','BloodPressure','SkinThickness','Insulin','BMI','DiabetesPedigreeFunction','Age']])
@@ -193,10 +155,6 @@ print("after using standard scalar and converting the negatives to absolute valu
 
 print(df)
 
-
-# In[81]:
-
-
 #shuffling the dataset
 shuffle_df = df.sample(frac=1,random_state=10)
 train_size = int(0.5*len(df))
@@ -207,33 +165,15 @@ test_set = shuffle_df[train_size:]
 trainset_List=train_set.values.tolist()
 testset_list=test_set.values.tolist()
 
-
-# In[82]:
-
-
-#predicting using euclidean distance measure and having k=6
+# predicting using euclidean distance measure and having k=6
 model = KNN()
 predictions = model.knn(trainset_List, testset_list)
-
-
-# In[83]:
-
-
 arr = model.check_best_k()
-
-
-# In[84]:
-
-
 k_arr=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 plt.figure()
 plt.plot(k_arr, arr)
 
-
-# # # Normalizing the train data after it is Pre-Processed
-
-# In[85]:
-
+# Normalizing the train data after it is Pre-Processed
 
 df=pd.read_csv('diabetes.csv')
 df2=pd.read_csv('diabetes.csv')
@@ -254,10 +194,10 @@ standardDf['Age']=standardDf['Age'].abs()
 standardDf['Outcome']=df['Outcome'].values
 df=standardDf
 
-#shuffling the dataset
+# shuffling the dataset
 shuffle_df=df.sample(frac=1,random_state=10)
 train_size=int(0.5*len(df))
-#splitting data into train and test
+# splitting data into train and test
 train_set=shuffle_df[:train_size]
 test_set=shuffle_df[train_size:]
 print("training set :",len(train_set)," rows")
@@ -268,11 +208,7 @@ print(train_set)
 print("test set obtained after standard scaling the dataset\n")
 print(test_set)
 
-
-# # Normalizing only the training set 
-
-# In[86]:
-
+# Normalizing only the training set 
 
 print("train_set after normalization\n")
 train_set=preprocessing.normalize(train_set[['Pregnancies','Glucose','BloodPressure','SkinThickness','Insulin','BMI','DiabetesPedigreeFunction','Age']])
@@ -280,112 +216,37 @@ train_set=pd.DataFrame(train_set,columns=column_names)
 train_set['Outcome']=ts['Outcome'].values
 print(train_set)
 
-
-# In[87]:
-
-
 trainset_List=train_set.values.tolist()
 testset_list=test_set.values.tolist()
 
-
-# In[88]:
-
-
-#prediction for k=6 and euclidean distance measure
-
-
-# In[89]:
-
+# prediction for k=6 and euclidean distance measure
 
 model = KNN()
 predictions = model.knn(trainset_List, testset_list)
-
-
-# In[90]:
-
-
 arr = model.check_best_k()
-
-
-# In[91]:
-
-
 k_arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 plt.figure()
 plt.plot(k_arr, arr)
 
+# Using different distance measure and different value for k
 
-# # Using different distance measure and different value for k
+# using the same test and train data since it has given the best accuracy
+# train data is standardized and normalized whereas test data is only standardized
 
-# In[101]:
-
-
-#using the same test and train data since it has given the best accuracy
-
-
-# In[93]:
-
-
-#train data is standardized and normalized
-
-
-# In[94]:
-
-
-#test data is only standardized
-
-
-# In[95]:
-
-
-#using manhattan distance instead of euclidean
+# using manhattan distance instead of euclidean
 model = KNN(metric = 'manhattan', k=5, p=2)
 predictions = model.knn(trainset_List, testset_list)
-
-
-# In[96]:
-
-
 arr = model.check_best_k()
-
-
-# In[97]:
-
-
 k_arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 plt.figure()
 plt.plot(k_arr, arr)
+plt.show()
 
-
-# In[69]:
-
-
-#using minkowski distance instead of euclidean
-
-
-# In[98]:
-
+# using minkowski distance instead of euclidean
 
 model = KNN(metric = 'minkowski', k=5, p=2)
 predictions = model.knn(trainset_List, testset_list)
-
-
-# In[99]:
-
-
 arr = model.check_best_k()
-
-
-# In[100]:
-
-
 k_arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 plt.figure()
 plt.plot(k_arr, arr)
-
-
-# In[ ]:
-
-
-
-
